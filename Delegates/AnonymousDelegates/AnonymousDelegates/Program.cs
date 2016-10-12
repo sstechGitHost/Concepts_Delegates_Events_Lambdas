9c7b3f50faeddaefd4cd6c78854dfcdea7265a4c
Nahidahmed <nahid.mansuri@gmail.com>
2016-10-12 14:56:21 +0530

    Basic concepts

---
+++ Delegates/AnonymousDelegates/AnonymousDelegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousDelegates
+{
+
+    public delegate string MyDelegate(int arg1, int arg2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SNIPPET GOES HERE
+            MyDelegate f = delegate (int arg1, int arg2) {
+                return (arg1 + arg2).ToString();
+            };
+            Console.WriteLine("The number is: " + f(10, 20));
+
+            // Keep the console window open until a key is pressed
+            Console.WriteLine("\nPress Enter Key to Continue...");
+            Console.ReadLine();
+        }
+    }
+}
